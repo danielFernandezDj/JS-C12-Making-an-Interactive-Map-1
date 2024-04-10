3010fa2acb9fe81e8135f9af1f65919e24e9d4a1
danielFernandezJd <mdanif9728@gmail.com>
2024-04-09 20:22:06 -0700

    regular saving

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,13 +1,71 @@
-let newCord = ''
 
-let map = L.map('map').setView([56.505, -0.09], 13);
+// let map = L.map('map').setView([56.505, -0.09], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-    .openPopup();
+// L.marker([51.5, -0.09]).addTo(map)
+//     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+//     .openPopup();
 
-// ! •••••••••••••••••••••••••••••••••••••••••••••••••••
+// ToDo ••••••••••••••••••••••••••••••••••••••••••••••••••
+// map object
+const myMap = {
+	coordinates: [],
+	businesses: [],
+	map: {},
+	markers: {},
+
+	// build leaflet map
+	buildMap() {
+		this.map = L.map('map', {
+		center: this.coordinates,
+		zoom: 13,
+		});
+		// add openstreetmap tiles
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution:
+			'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+		minZoom: '15',
+		}).addTo(this.map)
+		// create and add geolocation marker
+		const marker = L.marker(this.coordinates)
+		marker
+		.addTo(this.map)
+		.bindPopup('<p1><b>You are here</b><br></p1>')
+		.openPopup()
+	},
+
+	// add business markers
+}
+
+// get coordinates via geolocation API
+async function getCoords(){
+	const position = await new Promise((resolve, reject) => {
+		navigator.geolocation.getCurrentPosition(resolve, reject)
+	});
+	return [position.coords.latitude, position.coords.longitude]
+}
+
+// get foursquare businesses
+
+// process foursquare array
+
+
+// event handlers
+// window load
+window.onload = async () => {
+	const coords = await getCoords()
+	console.log(coords)
+	myMap.coordinates = coords
+	myMap.buildMap()
+}
+
+// Select the 'business' submit button and 'select' element.
+document.getElementById('submit').addEventListener('click', async (event) => {
+	event.preventDefault()
+    // Get the select element 
+	let business = document.getElementById('business').value
+	console.log(business)
+})
